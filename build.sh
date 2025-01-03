84c52233d417b9386a8a2529d234c3140a2214f1
kal-i <paul.maceres00@gmail.com>
2025-01-03 22:18:05 +0800

    Deleted unnecessary files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Flutter SDK
-git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
-
-# Run Flutter build
-flutter doctor
-flutter build web
